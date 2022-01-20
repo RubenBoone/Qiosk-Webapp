04882d9441eefef4de8e5fd6f5bc3996d15eccae
Ruben Boone <r0785519@student.thomasmore.be>
2022-01-20 17:41:30 +0100

    booking form stap 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookingFormComponent } from './booking/booking-form/booking-form.component';
 import { DatePickerComponent } from './booking/date-picker/date-picker.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'booking', component: DatePickerComponent },
+  { path: 'booking2', component: BookingFormComponent },
 ];
 
 @NgModule({
