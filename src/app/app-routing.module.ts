7b1846753d07d89968e98bc35c3c85f21c291172
Ruben Boone <r0785519@student.thomasmore.be>
2022-01-20 17:16:23 +0100

    booking stap 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DatePickerComponent } from './booking/date-picker/date-picker.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: "", component: HomeComponent},
-
-
+  { path: '', component: HomeComponent },
+  { path: 'booking', component: DatePickerComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
