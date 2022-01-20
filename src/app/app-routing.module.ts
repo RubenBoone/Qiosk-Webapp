1a8e1e7e0a49268d4e91ab25ddb753d48220d4c6
Ruben Boone <r0785519@student.thomasmore.be>
2022-01-20 17:57:45 +0100

    booking pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DatePickerComponent } from './booking/date-picker/date-picker.component';
+import { BookingFormComponent } from './booking/booking-form/booking-form.component';
 
 const routes: Routes = [
-  {path: "", component: HomeComponent},
-
-
+  { path: '', component: HomeComponent },
+  { path: 'booking', component: DatePickerComponent },
+  { path: 'booking2', component: BookingFormComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
