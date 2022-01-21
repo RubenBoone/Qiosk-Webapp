8234e445eea8768b03b9100970f67268925747a8
Ruben Boone <r0785519@student.thomasmore.be>
2022-01-21 12:22:59 +0100

    booking form 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BookingFormComponent } from './booking/booking-form/booking-form.component';
+import { MainBookingComponent } from './booking/main-booking.component';
 import { HomeComponent } from './home/home.component';
-import { DatePickerComponent } from './booking/date-picker/date-picker.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'booking', component: DatePickerComponent },
-  { path: 'booking2', component: BookingFormComponent },
+  { path: 'booking', component: MainBookingComponent },
 
   {
     path: 'admin',
