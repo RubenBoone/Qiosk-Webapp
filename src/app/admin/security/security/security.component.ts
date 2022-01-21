4d72030307add713a31c9d50aa679112a256dda3
Albert Baffour Awuah <baffour006@gmail.com>
2022-01-21 08:39:25 +0100

    auth/layout

---
+++ src/app/admin/security/security/security.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { EncryptionService } from '../encryption.service';
+import { User } from '../user';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-security',
+  templateUrl: './security.component.html',
+  styleUrls: ['./security.component.scss']
+})
+export class SecurityComponent implements OnInit {
+  CE = localStorage.getItem('email');
+  user: User = {
+    userID: 0, email: this.CE != null ? this.CE : '', password: '', token: '',
+    isActive: false,
+    isAdmin: false
+  };
+
+  isSubmitted: boolean = false;
+  errorMessage: string = '';
+
+  isLogin: boolean = false;
+  isLogout: boolean = false;
+  constructor(private authService: AuthService, private encryptionService: EncryptionService, private router: Router) {
+}
+  ngOnInit(): void {
+    switch (this.router.url) {
+      case '/admin/login': {
+        this.isLogin = true;
+        break;
+      }
+      case '/admin/logout': {
+        this.isLogout = true;
+        this.authService.logout();
+        this.router.navigate(['']);
+        break;
+      } default: {
+        this.isLogin = true;
+        break;
+      }
+    }
+  }
+  onSubmit(): void {
+    this.isSubmitted = true;
+    if (this.isLogin) {
+      this.authService.authenticate(this.user.email,this.encryptionService.encrypt(this.user.password)).subscribe(result => {
+        this.errorMessage = '';
+        this.authService.login(result);
+        this.getUser();
+      }, error => {
+        this.errorMessage = error.status == 0 ? 'Slechte verbinding... probeer de pagina opnieuw te laden!' : 'Email of wachtwoord is onjuist!';
+        this.isSubmitted = false;
+      });
+    }
+  }
+  getUser() {
+    let user = this.authService.getUser();
+    if (user != null) {
+      this.user = user;
+    }
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { NavigationMobileComponent } from './navigation-mobile/navigation-mobile.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HomeComponent,
-    NavigationComponent,
-    NavigationMobileComponent,
+    NavigationComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    SharedModule,
+    AppRoutingModule,
+    HttpClientModule
+
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
