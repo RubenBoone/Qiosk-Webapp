7ac601dc22408e5865ef4ca75b8cacc025565d9e
Ruben Boone <r0785519@student.thomasmore.be>
2022-01-25 09:04:39 +0100

    booking service

---
+++ src/app/admin/bookings-table/booking.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GlobalConstants } from 'src/app/shared/global-constants';
+
+import { Booking } from './booking';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookingService {
+  baseUrl = GlobalConstants.apiURL;
+  constructor(private httpClient: HttpClient) {}
+
+  getBookings(): Observable<Booking[]> {
+    return this.httpClient.get<Booking[]>(this.baseUrl + 'Bookings');
+  }
+
+  getBooking(id: number): Observable<Booking> {
+    return this.httpClient.get<Booking>(this.baseUrl + 'Bookings/' + id);
+  }
+
+  postBooking(booking: Booking): Observable<Booking> {
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+
+    return this.httpClient.post<Booking>(this.baseUrl + '/bookings', booking, {
+      headers: headers,
+    });
+  }
+
+  putBooking(id: number, booking: Booking): Observable<Booking> {
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+
+    return this.httpClient.put<Booking>(
+      this.baseUrl + 'Bookings/' + id,
+      booking,
+      { headers: headers }
+    );
+  }
+
+  deleteBooking(id: number): Observable<Booking> {
+    return this.httpClient.delete<Booking>(this.baseUrl + 'Bookings/' + id);
+  }
+}
---
+++ src/app/admin/bookings-table/booking.ts
@@ -0,0 +1,4 @@
+export interface Booking {
+  bookingID: number;
+  bookingTime: Date;
+}
