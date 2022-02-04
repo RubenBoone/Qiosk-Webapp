ed915426c23b7920102ee71def884cef60050a9a
Stef-Van-Gool <r0740461@student.thomasmore.be>
2022-02-04 13:01:00 +0100

    ww change + scroll again.... :/

--- src/app/appointment/appointment.module.ts
+++ src/app/appointment/appointment.module.ts
@@ -4,9 +4,10 @@ import { MainComponent } from './main/main.component';
 import { BookingFormComponent } from './booking-form/booking-form.component';
 import { DatePickerComponent } from './date-picker/date-picker.component';
 import { SharedModule } from '../shared/shared.module';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 
 @NgModule({
-  declarations: [MainComponent, BookingFormComponent, DatePickerComponent],
+  declarations: [MainComponent, BookingFormComponent, DatePickerComponent, ConfirmationComponent],
   imports: [SharedModule],
   exports: [MainComponent, BookingFormComponent, DatePickerComponent],
 })
