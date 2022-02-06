2133669138fd143239271f815cc2333be288d8b4
Albert Baffour Awuah <baffour006@gmail.com>
2022-02-06 16:54:20 +0100

    booking inorde1

--- src/app/shared/global-constants.ts
+++ src/app/shared/global-constants.ts
@@ -1,4 +1,4 @@
 export class GlobalConstants {
-  //public static apiURL: string = 'https://qioskdotnetapi.azurewebsites.net/api/';
-  public static apiURL: string = "https://localhost:44354/api/";
+  public static apiURL: string = 'https://qioskdotnetapi.azurewebsites.net/api/';
+  //public static apiURL: string = "https://localhost:44354/api/";
 }
